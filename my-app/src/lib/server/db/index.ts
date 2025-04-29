bf8f73995a90330d5125885bd494ca2fa1617988
zs538 <zstulhofer81@gmail.com>
2025-04-29 11:18:08 +0200

    reinit + db

--- my-app/src/lib/server/db/index.ts
+++ my-app/src/lib/server/db/index.ts
@@ -1,10 +1,6 @@
-import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2/promise';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+import Database from 'better-sqlite3';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-const client = mysql.createPool(env.DATABASE_URL);
-
-export const db = drizzle(client, { schema, mode: 'default' });
+const sqlite = new Database('my-app.sqlite');
+export const db = drizzle(sqlite, { schema });
