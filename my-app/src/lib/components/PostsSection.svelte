887bff1d26d48d4f5645264d962c35b031185b67
zs538 <zstulhofer81@gmail.com>
2025-04-29 13:07:04 +0200

    docs

---
+++ my-app/src/lib/components/PostsSection.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { Post } from '$lib/server/db/schema';
+    import PostComponent from './Post.svelte'; // Use regular import and a different local name
+    export let posts: Post[];
+  </script>
+  
+  <section>
+    {#each posts as post}
+      <PostComponent {post} />
+    {/each}
+  </section>
--- my-app/src/routes/homepage/+page.svelte
+++ my-app/src/routes/homepage/+page.svelte
@@ -1,13 +1,7 @@
 <script>
-  export let data;
+  import PostsSection from '$lib/components/PostsSection.svelte';
+  export let data; // Provided by +page.server.ts
 </script>
 
 <h1>Homepage Feed</h1>
-
-{#each data.posts as post}
-  <article>
-    <h2>{post.text}</h2>
-    <p>By user ID: {post.authorId}</p>
-    <!-- You can add more post details here, such as createdAt, etc. -->
-  </article>
-{/each}
+<PostsSection posts={data.posts} />
