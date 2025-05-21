9b6580b6b062085e3ee672db9ada201c8d8de912
zs538 <zstulhofer81@gmail.com>
2025-05-21 11:26:10 +0200

    portable

---
+++ my-app/run-node.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Set path to portable Node.js
+SET NODE_PATH=G:\nodejs\node-v22.15.1-win-x64
+SET PATH=%NODE_PATH%;%PATH%
+
+REM Run node command with all arguments passed to this batch file
+%NODE_PATH%\node.exe %*
---
+++ my-app/run-npm.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Set path to portable Node.js
+SET NODE_PATH=G:\nodejs\node-v22.15.1-win-x64
+SET PATH=%NODE_PATH%;%PATH%
+
+REM Make sure node is also in the PATH
+SET NODE_BIN=%NODE_PATH%
+SET PATH=%NODE_BIN%;%PATH%
+
+REM Run npm command with all arguments passed to this batch file
+%NODE_PATH%\npm.cmd %*
+
+REM If you need to see the environment for debugging
+REM echo PATH=%PATH%
